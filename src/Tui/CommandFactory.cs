8eb7a8abe652d457482303bf5758bfd2b4f9fc77
Mastergammler <mg.coffee.man.goa@gmail.com>
2025-07-12 13:35:52 +0200

    close #20 - Tag filter for time cmd

--- src/Tui/CommandFactory.cs
+++ src/Tui/CommandFactory.cs
@@ -75,7 +75,7 @@ public class CommandFactory : ICommandFactory
             Type t when t == typeof(StartTrackingCmd) => new StartTrackingCmd(_itemService, _timeService),
             Type t when t == typeof(StopTrackingCmd) => new StopTrackingCmd(_timeService),
             Type t when t == typeof(TrackingInfoCmd) => new TrackingInfoCmd(_timeRepository),
-            Type t when t == typeof(TimeOverviewCmd) => new TimeOverviewCmd(_timeRepository),
+            Type t when t == typeof(TimeOverviewCmd) => new TimeOverviewCmd(_timeRepository, _tags),
             Type t when t == typeof(TimeFillCmd) => new TimeFillCmd(_itemService, _timeService),
             Type t when t == typeof(ListEntriesCmd) => new ListEntriesCmd(_db),
 
--- src/Tui/Commands/DbStatsCmd.cs
+++ src/Tui/Commands/DbStatsCmd.cs
@@ -5,13 +5,17 @@ public class DbStatsCmd(SttContext db) : ICommand
     public void Execute(Memory<string> args)
     {
         int itemCount = db.Items.Count();
+        int projectCount = db.Projects.Count();
+        int taskCount = db.Tasks.Count();
         int tagCount = db.Tags.Count();
         int filterCount = db.Filters.Count();
+        int timeEntries = db.TimeEntries.Count();
 
         Print($"The db holds:");
-        Print($"- {itemCount} items");
+        Print($"- {itemCount} items ({taskCount} tasks, {projectCount} Projects)");
         Print($"- {tagCount} tags");
         Print($"- {filterCount} filters");
+        Print($"- {timeEntries} time entries");
         Print("Console info:");
         Print($"- {Console.LargestWindowWidth} Max");
         Print($"- {Console.WindowWidth} Current");
